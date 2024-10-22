f8cb709404a0e948ce3855a0cfbe66c72ed76187
TaQuangTruc <truc.taquang23904@hcmut.edu.vn>
2024-10-22 07:40:10 +0700

    add api

--- thulinhsinhvien_app/src/utils/exam.js
+++ thulinhsinhvien_app/src/utils/exam.js
@@ -1,9 +1,22 @@
 import { readData, writeData } from "./firebase"
 
+// Khi chuyển trang câu hỏi bằng các phím mũi tên (-> bà <-), cập nhật lại chỉ số câu hỏi
 export async function updateIndexQuestion(index) {
     try {
-        await writeData("questionIndex", index);
+        await writeData("exam/questionIndex", index);
     } catch (error) {
         console.log("Lỗi khi cập nhật câu hỏi")
     }
 }
+
+// Khi chuyển trang câu hỏi bằng các phím mũi tên (-> bà <-), cập nhật isRun thành false
+// Khi ấn show câu hỏi => cập nhật isRun thành true
+export async function updateIsRun(option) {
+    try {
+        await writeData("exam/isRun", option);
+    } catch (error) {
+        console.log("Lỗi khi cập nhật isRun")
+        return null;
+    }
+}
+
